55762141db0e860bb7a7fc978653339feee094b9
Mohil-Mehra <mohil123m@gmail.com>
2025-11-10 15:52:07 +0530

    added signup components and fixed form state

--- frontend/src/pages/captainsignup.jsx
+++ frontend/src/pages/captainsignup.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React , { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const captainsignup = () => {
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+  const [Firstname , setFirstname] = useState("");
+  const [Lastname , setLastname] = useState("");
+  const [userData , setUserData] = useState({});
+  
+
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({  fullname: { Firstname:Firstname , Lastname:Lastname},email:email , password:password});
+    console.log(userData);
+    setFirstname("");
+    setLastname("");
+    setEmail("");
+    setPassword("");
+  }
   return (
-    <div>captainsignup</div>
+  <div>
+    <div>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt=""  className='w-[60px] h-[60px] object-contain  ml-4'/>
+          
+   <form className='p-7'  onSubmit={(e)=>{submitHandler(e)}} >
+    <h3 className='text-xl mb-2'>What's your Name ?</h3>
+  <div className='flex gap-4 mb-5'>
+    <input required type="text" value={Firstname} onChange={(e) => {setFirstname(e.target.value) }} className='bg-[#eeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base ' placeholder="First name" />
+    <input required type="text" value={Lastname} onChange={(e) => {setLastname(e.target.value) }} className='bg-[#eeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base ' placeholder="Last name"/>
+  </div>
+   <h3 className='text-xl mb-2'>What's your email?</h3>
+    <input required type="email" value={email} onChange={(e) => {setEmail(e.target.value) }}  className='bg-[#eeee] mb-5 rounded px-4 py-2 border w-full text-lg placeholder:text-base ' placeholder="Email@eg.com" />
+    <h3 className='text-xl mb-2'>Enter your password</h3>
+    <input required type="password"   value={password} onChange={(e) => {setPassword(e.target.value) }} className='bg-[#eeee] rounded px-4 py-2 border mb-5 w-full text-lg placeholder:text-base ' placeholder="Password" />
+    <button className='bg-[#111] font-semibold mt-4 text-white rounded px-4 py-2  w-full text-lg placeholder:text-base '>Login</button>
+   <p className='text-sm mb-5 mt-1'>Already have a account? <Link to="/captainlogin" className='text-blue-500 '>Login here </Link>  </p>
+   </form>
+   
+    </div>
+    <div className='mt-[-10px] flex justify-center ml-7 mr-7 '>
+   <p className='mt-[10vh]  text-[10px]' >By creating an account, you agree to the Terms and Conditions and Privacy Notice </p>
+    </div>
+  </div>
   )
 }
 
--- frontend/src/pages/usersignup.jsx
+++ frontend/src/pages/usersignup.jsx
@@ -6,14 +6,17 @@ const usersignup = () => {
   const [password , setPassword] = useState("");
   const [Firstname , setFirstname] = useState("");
   const [Lastname , setLastname] = useState("");
+  const [userData , setUserData] = useState({});
   
 
 
 
   const submitHandler = (e) => {
     e.preventDefault();
-    setUserData({email:email , password:password});
+    setUserData({  fullname: { Firstname:Firstname , Lastname:Lastname},email:email , password:password});
     console.log(userData);
+    setFirstname("");
+    setLastname("");
     setEmail("");
     setPassword("");
   }
@@ -25,13 +28,13 @@ const usersignup = () => {
    <form className='p-7'  onSubmit={(e)=>{submitHandler(e)}} >
     <h3 className='text-xl mb-2'>What's your Name ?</h3>
   <div className='flex gap-4 mb-5'>
-    <input required type="text"   className='bg-[#eeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base ' placeholder="First name" />
-    <input required type="text"   className='bg-[#eeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base ' placeholder="Last name"/>
+    <input required type="text" value={Firstname} onChange={(e) => {setFirstname(e.target.value) }} className='bg-[#eeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base ' placeholder="First name" />
+    <input required type="text" value={Lastname} onChange={(e) => {setLastname(e.target.value) }} className='bg-[#eeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base ' placeholder="Last name"/>
   </div>
    <h3 className='text-xl mb-2'>What's your email?</h3>
     <input required type="email" value={email} onChange={(e) => {setEmail(e.target.value) }}  className='bg-[#eeee] mb-5 rounded px-4 py-2 border w-full text-lg placeholder:text-base ' placeholder="Email@eg.com" />
     <h3 className='text-xl mb-2'>Enter your password</h3>
-    <input required type="password"  className='bg-[#eeee] rounded px-4 py-2 border mb-5 w-full text-lg placeholder:text-base ' placeholder="Password" />
+    <input required type="password"   value={password} onChange={(e) => {setPassword(e.target.value) }} className='bg-[#eeee] rounded px-4 py-2 border mb-5 w-full text-lg placeholder:text-base ' placeholder="Password" />
     <button className='bg-[#111] font-semibold mt-4 text-white rounded px-4 py-2  w-full text-lg placeholder:text-base '>Login</button>
    <p className='text-sm mb-5 mt-1'>Already have a account? <Link to="/userlogin" className='text-blue-500 '>Login here </Link>  </p>
    </form>
